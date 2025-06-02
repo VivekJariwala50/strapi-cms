7ecac1250f4321ed329e7ce85cee757f02271bc3
vivekjariwala50 <vivekjariwala50@gmail.com>
2025-06-01 20:57:47 -0400

    again push

---
+++ dist/src/api/vivek-jariwala/routes/vivek-jariwala.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * vivek-jariwala router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::vivek-jariwala.vivek-jariwala');
---
+++ dist/src/api/vivek-jariwala/services/vivek-jariwala.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * vivek-jariwala service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::vivek-jariwala.vivek-jariwala');
